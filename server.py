81f9ffd404722c8bb6011463eb395188f8855a38
ainunfashiaa <158124428+ainunfashiaa@users.noreply.github.com>
2024-06-15 09:55:40 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route('/post-data', methods=['POST', 'GET'])
-def post_data():
-    data = request.json
-    if not data:
-        return jsonify({'error': 'No data provided'}), 400
-    
-    temperature = data.get('temperature')
-    humidity = data.get('humidity')
-    
-    if temperature is None or humidity is None:
-        return jsonify({'error': 'Invalid data'}), 400
+base = []
 
-    print(f"Received data - Temperature: {temperature}, Humidity: {humidity}")
-    
-    return jsonify({'message': 'Data received successfully'}), 200
+@app.route('/post-data', methods=['POST'])
+def receive_data():
+    global base
+    data = request.get_json()
+    lux = data['lux']
+    SubData = [lux]
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    base = SubData
+
+    print("Data diterima intensitas cahaya:", lux, "lux ")
+    
+    response = {'message': 'Data diterima'}
+    return jsonify(response), 200
+
+@app.route('/post-data', methods=['GET'])
+def dapat_data():
+    global base
+    return jsonify({'lux': base[0],}) if base else jsonify({'message': 'No data available'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
